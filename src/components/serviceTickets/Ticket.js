f974266777ba3956a04649efb466695f56ccda37
Steve Brownlee <chortlehoort@gmail.com>
2021-07-31 19:20:06 -0500

    Edit ticket to assign employee

--- src/components/serviceTickets/Ticket.js
+++ src/components/serviceTickets/Ticket.js
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from "react"
-import { useHistory, useParams } from "react-router-dom"
+import { useParams } from "react-router-dom"
 
 export const Ticket = () => {
     const [ticket, set] = useState({})
     const [employees, syncEmployees] = useState([])
     const { ticketId } = useParams()
-    const history = useHistory()
 
-
-    useEffect(() => {
-        fetch(`http://localhost:8088/serviceTickets/${ticketId}?_expand=customer&_expand=employee`)
+    const fetchTicket = () => {
+        return fetch(`http://localhost:8088/serviceTickets/${ticketId}?_expand=customer&_expand=employee`)
             .then(res => res.json())
             .then(set)
-    }, [ticketId])
+    }
+
+    useEffect(() => {
+        fetchTicket()
+    }, [ ticketId ])
 
     useEffect(
         () => {
@@ -30,26 +32,28 @@ export const Ticket = () => {
                 <select
                     value={ticket.employeeId}
                     onChange={
-                    evt => {
-                        const copy = { ...ticket }
-                        copy.employeeId = parseInt(evt.target.value)
-                        delete copy.customer
-                        delete copy.employee
+                        evt => {
+                            const updatedTicket = {
+                                customerId: ticket.customerId,
+                                employeeId: parseInt(evt.target.value),
+                                description: ticket.description,
+                                emergency: ticket.emergency,
+                                dateCompleted: new Date().toLocaleDateString("en-US")
+                            }
 
-                        fetch(`http://localhost:8088/serviceTickets/${ticketId}`, {
-                            method: "PUT",
-                            headers: {
-                                "Content-Type": "application/json"
-                            },
-                            body: JSON.stringify(copy)
-                        }).then(() => {
-                            set(copy)
-                            history.push(`/tickets/${ticketId}`)
-                        })
-                    }
-                }>
+                            fetch(`http://localhost:8088/serviceTickets/${ticketId}`, {
+                                method: "PUT",
+                                headers: {
+                                    "Content-Type": "application/json"
+                                },
+                                body: JSON.stringify(updatedTicket)
+                            }).then(() => {
+                                fetchTicket()
+                            })
+                        }
+                    }>
                     {
-                        employees.map(e => <option value={e.id}>{e.name}</option>)
+                        employees.map(e => <option key={`employee--${e.id}`} value={e.id}>{e.name}</option>)
                     }
                 </select>
             </div>
